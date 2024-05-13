36615c495ebbca86cd0aa1512c6c2da5e0a36bb0
bradreelee <chantree607@gmail.com>
2024-05-13 18:57:51 +0900

    build 메소드안에서 future 생성

--- lib/main.dart
+++ lib/main.dart
@@ -37,32 +37,34 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
+      floatingActionButton: TextButton(
+        onPressed: () => setState(() {}),
+        child: const Text('refetch'),
+      ),
       body: FutureBuilder<String>(
-          future: fetchTodo(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: Colors.grey,
-                ),
-              );
-            }
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Center(child: Text(snapshot.data!));
-            }
+        future: fetchTodo(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(
-              child: Text("error while fetching todo"),
+              child: CircularProgressIndicator(
+                color: Colors.grey,
+              ),
             );
-          }),
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Center(child: Text(snapshot.data!));
+          }
+          return const Center(
+            child: Text("error while fetching todo"),
+          );
+        },
+      ),
     );
   }
 
   Future<String> fetchTodo() async {
     const url = 'https://jsonplaceholder.typicode.com/todos/1';
     final response = await http.get(Uri.parse(url));
-    setState(() {
-      // 임의의 로직
-    });
     return response.body;
   }
 }
